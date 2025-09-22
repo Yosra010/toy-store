cb56046a21cee13fafc42ea0e1d5a2922f01434a
yosraelmasry-dev <74475192+Yosra010@users.noreply.github.com>
2025-09-22 15:22:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function completePurchase() {
-  const method = document.querySelector('input[name="payment"]:checked');
-  if (!method) {
-    alert("من فضلك اختر وسيلة الدفع");
-    return;
-  }
-  alert("تمت عملية الشراء بنجاح ✅");
-}
